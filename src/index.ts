7ef9a745b4fd823b9bf5e4f7e111f7f40b340fc3
Jonathan Springer <jonpspri@gmail.com>
2023-03-06 11:35:32 -0500

    Logging ready-to-go

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,40 @@
+#!/usr/bin/env node
 import { Command } from 'commander'
-import figlet from 'figlet'
 import Conf from 'conf'
+import { pino } from 'pino'
+
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone.js'
+import advancedFormat from 'dayjs/plugin/advancedFormat.js'
+dayjs.extend(timezone)
+dayjs.extend(advancedFormat)
 
 import { stream } from './ring-streamer.js'
 
+console.log(process.env.LOG_FILE)
+
+export var logger : pino.Logger
+
+const version = process.env.npm_package_version as string
 const program = new Command()
 const config = new Conf({projectName: 'ring-server'})
-
-console.log(figlet.textSync('Ring Server'));
 
 program
   .name('ring-server')
   .description('CLI to manage server-like Ring activities')
-  .version('0.0.1')
+  .version(version)
+  .hook('preAction', (_thisCommand, actionCommand) => {
+    //  This happens in a preAction hook because a parsing exit is too fast
+    //  for the log to keep up with and results to help to Stdout anyway
+    logger = pino({
+      name: 'ring-streamer',
+      level: process.env.LEVEL || 'info'
+    }, pino.destination(process.env.LOG_FILE || 1))
+
+    logger.info(`Ring Server version ${version}`)
+    logger.info(`Starting at ${dayjs().format('MMMM D YYYY, HH:mm:ss z')}`)
+    logger.debug(`Executing ${actionCommand.name()}`)
+  })
 
 program.command('seed-token')
   .description('Seed the Ring token with the provided value')
@@ -22,10 +44,11 @@ program.command('seed-token')
 program.command('serve-camera')
   .description('Serve video from the given cameras via NGinx')
   .option('-t, --token <token>', 'Ring token for initial request')
-  .argument('<camera...>', 'The name(s) of the camera(s) to server')
+  .argument('<camera>', 'The name(s) of the camera(s) to server')
   .action((camera, options, _command) => {
     if (options.token) { config.set('token', options.token) }
     stream(camera)
   })
 
 program.parse(process.argv)
+
--- src/ring-streamer.ts
+++ src/ring-streamer.ts
@@ -1,58 +1,42 @@
 import { RingApi, RingCamera } from 'ring-client-api'
 import Conf from 'conf'
+import { logger } from './index.js'
 
 const config = new Conf({projectName: 'ring-server'})
 
-// TODO:  Put some proper log levels into place to reduce console verbosity.
-
 async function startCamera(camera: RingCamera) {
-  console.log(`(Re)starting call for ${camera.name}`)
+  logger.debug(`(Re)starting video for ${camera.name}`)
   const call = await camera.streamVideo({
     audio: [ '-af', 'volume=0.0' ],  // Mute for privacy
-    video: [
-      '-vcodec', 'libx264',
+    video: [ '-vcodec', 'libx264',
       '-vprofile', 'baseline',
-      '-vf', 'scale=640x360',
-    ],
-    output: [
-      '-f', 'flv',
-      `rtmp://127.0.0.1/live/${camera.name.replace(/ /g,"_")}`
-    ],
+      '-vf', 'scale=640x360', ],
+    output: [ '-f', 'flv',
+      `rtmp://127.0.0.1/live/${camera.name.replace(/ /g,"_")}` ],
   })
-  console.log(`Video started for ${camera.name}...`)
+  logger.info(`Video started for ${camera.name}`)
 
   call.onCallEnded.subscribe(() => {
-    console.log(`Call has ended for ${camera.name}`)
+    logger.debug(`Call has ended for ${camera.name}`)
     startCamera(camera)
   })
 }
 
-export async function stream(desired_cameras: string[]) {
+export async function stream(desired_camera: string) {
   const ringApi = new RingApi({
-      // Replace with your refresh token
       refreshToken: config.get('token') as string,
       debug: true,
-    }),
-    cameras = await ringApi.getCameras()
-
-  // Refresh token periodically
+    })
   ringApi.onRefreshTokenUpdated.subscribe(
-    async ({ newRefreshToken}) => { config.set('token', newRefreshToken) }
+    async ({ newRefreshToken }) => { config.set('token', newRefreshToken) }
   )
 
-  if (cameras.length == 0) {
-    console.log('No cameras found')
-    return
+  const camera = (await ringApi.getCameras()).find( x => x.name === desired_camera );
+  if (!camera) {
+    logger.error(`The camera ${camera} is not supported by the Ring API`);
+    return 1;
   }
 
-  for (var camera of cameras) {
-    if (desired_cameras.indexOf(camera.name) < 0) {
-      console.log(`Skipping camera ${camera.name}...`)
-      continue
-    }
-
-    console.log(`Starting Video for ${camera.name}...`)
-    startCamera(camera)
-  }
+  startCamera(camera)
 }
 
